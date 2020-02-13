d1f9e45407c9f16d8c401dd06638297ffed3b2ac
Gianmaria Del Monte <g.macmount@gmail.com>
2020-02-13 19:56:40 +0100

    test

---
+++ test.jl
@@ -0,0 +1,17 @@
+using BenchmarkTools
+using Random
+
+sparseN(N) = sparse(randperm(N), randperm(N), ones(Int64, N), N, N)
+
+A = sparseN(100000)
+B = sparseN(100000)
+
+m1 = @btime $A*$B'
+
+As = sm2gbm(A)
+Bs = sm2gbm(B)
+mgb = @btime mm($As, $Bs)
+
+m2 = gbm2sm(mgb)
+
+@assert m1 == m2
