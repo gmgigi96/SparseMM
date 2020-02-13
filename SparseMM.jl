ee24cd6a0981dc9abe054ac6306fd281cbd6116e
Gianmaria Del Monte <g.macmount@gmail.com>
2020-02-13 19:56:30 +0100

    refactoring

--- SparseMM.jl
+++ SparseMM.jl
@@ -1,6 +1,13 @@
 using GraphBLASInterface, SuiteSparseGraphBLAS
 using SparseArrays
 
+GrB_init(GrB_NONBLOCKING)
+
+const desc = GrB_Descriptor()
+GrB_Descriptor_new(desc)
+GrB_Descriptor_set(desc, GrB_INP1, GrB_TRAN)
+#GrB_Descriptor_set(desc, GrB_OUTP, GrB_REPLACE)
+
 function sm2gbm(A::SparseMatrixCSC{Int64, Int64})
     res = GrB_Matrix{Int64}()
     GrB_Matrix_new(res, GrB_INT64, size(A, 1), size(A, 2))
@@ -16,18 +23,29 @@ function gbm2sm(A::GrB_Matrix{Int64})
     return SparseArrays.sparse(I, J, X)
 end
 
+function gbm_new(r, c)
+    C = GrB_Matrix{Int64}()
+    GrB_Matrix_new(C, GrB_INT64, r, c)
+    return C
+end
+
 function mm(A::GrB_Matrix{Int64}, B::GrB_Matrix{Int64})
 
-    desc = GrB_Descriptor()
-    GrB_Descriptor_new(desc)
-    GrB_Descriptor_set(desc, GrB_INP1, GrB_TRAN)
-    GrB_Descriptor_set(desc, GrB_OUTP, GrB_REPLACE)
+    #C = GrB_Matrix{Int64}()
+    #GrB_Matrix_new(C, GrB_INT64, GrB_Matrix_nrows(A), GrB_Matrix_ncols(B))
 
-    C = GrB_Matrix{Int64}()
-    GrB_Matrix_new(C, GrB_INT64, GrB_Matrix_nrows(A), GrB_Matrix_ncols(B))
+    C = gbm_new(GrB_Matrix_nrows(A), GrB_Matrix_ncols(B))
 
+    #GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
     GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
 
     return C
+end
 
+function mm!(A::GrB_Matrix{Int64}, B::GrB_Matrix{Int64}, C::GrB_Matrix{Int64})
+
+    #GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
+    GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
+
+    return C
 end
