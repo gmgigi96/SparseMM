998a36653ca59ebf115e6ebe582e045500911764
Gianmaria Del Monte <g.macmount@gmail.com>
2020-02-13 20:05:31 +0100

    removed descriptor graphblas

--- SparseMM.jl
+++ SparseMM.jl
@@ -3,11 +3,6 @@ using SparseArrays
 
 GrB_init(GrB_NONBLOCKING)
 
-const desc = GrB_Descriptor()
-GrB_Descriptor_new(desc)
-GrB_Descriptor_set(desc, GrB_INP1, GrB_TRAN)
-#GrB_Descriptor_set(desc, GrB_OUTP, GrB_REPLACE)
-
 function sm2gbm(A::SparseMatrixCSC{Int64, Int64})
     res = GrB_Matrix{Int64}()
     GrB_Matrix_new(res, GrB_INT64, size(A, 1), size(A, 2))
@@ -37,7 +32,7 @@ function mm(A::GrB_Matrix{Int64}, B::GrB_Matrix{Int64})
     C = gbm_new(GrB_Matrix_nrows(A), GrB_Matrix_ncols(B))
 
     #GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
-    GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
+    GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, GrB_NULL)
 
     return C
 end
@@ -45,7 +40,7 @@ end
 function mm!(A::GrB_Matrix{Int64}, B::GrB_Matrix{Int64}, C::GrB_Matrix{Int64})
 
     #GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
-    GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, desc)
+    GrB_mxm(C, GrB_NULL, GrB_NULL, GxB_PLUS_TIMES_INT64, A, B, GrB_NULL)
 
     return C
 end
--- test.jl
+++ test.jl
@@ -9,7 +9,7 @@ B = sparseN(100000)
 m1 = @btime $A*$B'
 
 As = sm2gbm(A)
-Bs = sm2gbm(B)
+Bs = sm2gbm(sparse(B'))
 mgb = @btime mm($As, $Bs)
 
 m2 = gbm2sm(mgb)
