5e2f265471772189a6df64a68f7091da846c2a6e
ramorimo <ramorimo90@hotmail.com>
2020-02-16 16:19:40 +0100

    (test) created a test to identify Division By Zero error

--- test.jl
+++ test.jl
@@ -14,7 +14,11 @@ function delta_3(M_2, M_3)
 	s = sum(M_2,dims=2)
 	d = (M_2 * M_3')
 	res = d ./ s
-	return res .÷ 1
+	res = res .÷ 1
+
+	res = map(x -> if isnan(x) 0 else x end, res)
+
+	return res
 end
 
 function test_d1_random_matrix()
@@ -91,7 +95,19 @@ function test_div_by_two()
 	return gbm2sm(B_div_2)
 end
 
+# ************************************************************************************ #
+randSparse(N) = sparse(randperm(N), randperm(N), rand(0:N, N), N, N)
+
+const A3 = sparse([0 3 0; 2 0 0; 0 0 0])
+const B3 = sparse([0 0 0; 0 1 0; 0 0 0])
+
+function test_d3_random_values_2()
+	global A3, B3
+	return d3(A3,B3)
+end
+
 @testset "SparseMM test" begin
+	@test test_d3_random_values_2() == delta_3(A3, B3)
 	@test test_d1_random_matrix() == A*B'
 	@test test_d2_random_matrix() == (A * B') .÷ 2
 	@test test_d3_random_matrix() == delta_3(A, B)
