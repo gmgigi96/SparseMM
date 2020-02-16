080d75e58eccc941d1d0777888c93df18531087c
ramorimo <ramorimo90@hotmail.com>
2020-02-16 16:20:03 +0100

    (bugfix) fixed Division By Zero error

--- SparseMM.jl
+++ SparseMM.jl
@@ -11,11 +11,13 @@ const TRAN_INP1 = GrB_Descriptor()
 GrB_Descriptor_new(TRAN_INP1)
 GrB_Descriptor_set(TRAN_INP1, GrB_INP1, GrB_TRAN)
 
+const int_div = (a, b) -> if a == 0 || b == 0 0 else a ÷ b end
+
 const DIV_INT64 = GrB_BinaryOp()
-GrB_BinaryOp_new(DIV_INT64, div, GrB_INT64, GrB_INT64, GrB_INT64)
+GrB_BinaryOp_new(DIV_INT64, int_div, GrB_INT64, GrB_INT64, GrB_INT64)
 
 const DIV_INT8 = GrB_BinaryOp()
-GrB_BinaryOp_new(DIV_INT8, div, GrB_INT8, GrB_INT8, GrB_INT8)
+GrB_BinaryOp_new(DIV_INT8, int_div, GrB_INT8, GrB_INT8, GrB_INT8)
 
 const db2 = x -> x÷2
 
