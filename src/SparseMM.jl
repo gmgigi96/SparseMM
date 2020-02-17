70544d5bc4b84a75c69543c4025b35a15242fdb6
Gianmaria Del Monte <g.macmount@gmail.com>
2020-02-17 19:54:29 +0100

    (doc) Documentation of SparseMM.jl functions

--- src/SparseMM.jl
+++ src/SparseMM.jl
@@ -6,6 +6,15 @@ include("Utils.jl")
 
 GrB_init(GrB_NONBLOCKING)
 
+"""
+    sm2gbm(A, tran=false)
+
+Convert a SparseMatrix into a GraphBLAS matrix.
+
+# Arguments
+- `A::SparseMatrixCSC`  : Sparse Matrix
+- `tran::Bool`          : if true trasposes the matrix before convertion
+"""
 function sm2gbm(A::SparseMatrixCSC{TE, TI}, tran=false) where {TE, TI}
     res = GrB_Matrix{TE}()
     if tran
@@ -19,6 +28,14 @@ function sm2gbm(A::SparseMatrixCSC{TE, TI}, tran=false) where {TE, TI}
     return res
 end
 
+"""
+    gbm2sm(A)
+
+Convert a GraphBLAS matrix into a SparseMatrix.
+
+# Arguments
+- `A::GrB_Matrix` : the GraphBLAS matrix
+"""
 function gbm2sm(A::GrB_Matrix)
     I, J, X = GrB_Matrix_extractTuples(A)
     I = map(e -> e.x+1, I)
@@ -26,6 +43,11 @@ function gbm2sm(A::GrB_Matrix)
     return SparseArrays.sparse(I, J, X, GrB_Matrix_nrows(A), GrB_Matrix_ncols(A))
 end
 
+"""
+    gbm_new(T, r, c)
+
+Create a GraphBLAS matrix with `r` row and `c` columns, with element of type `T`.
+"""
 function gbm_new(T, r, c)
     C = GrB_Matrix{T}()
     GrB_Matrix_new(C, GrB_type(T), r, c)
@@ -38,6 +60,11 @@ function gbv_new(T, l)
     return V
 end
 
+"""
+    mm(A, B)
+
+Multiply the GraphBLAS matrix A by the GraphBLAS matrix B.
+"""
 function mm(A::GrB_Matrix{T}, B::GrB_Matrix{T}) where T
     @assert GrB_Matrix_ncols(A) == GrB_Matrix_nrows(B)
     C = gbm_new(T, GrB_Matrix_nrows(A), GrB_Matrix_ncols(B))
@@ -47,12 +74,22 @@ function mm(A::GrB_Matrix{T}, B::GrB_Matrix{T}) where T
     return C
 end
 
+"""
+    sm(A)
+
+Sum rows of GraphBLAS matrix A.
+"""
 function sm(A::GrB_Matrix{T}) where T
     V = gbv_new(T, size(A, 1))
     GrB_reduce(V, GrB_NULL, GrB_NULL, GxB_monoid("PLUS", T), A, GrB_NULL)
     return V
 end
 
+"""
+    v2m(V, M)
+
+Multiply vector V of type T by the matrix M of type T.
+"""
 function v2m(V::GrB_Vector{T}, j, M::GrB_Matrix{T}) where T
     GrB_Matrix_clear(M)
     I, X = GrB_Vector_extractTuples(V)
@@ -61,6 +98,11 @@ function v2m(V::GrB_Vector{T}, j, M::GrB_Matrix{T}) where T
     return M
 end
 
+"""
+    dmv(A,B)
+
+Divide the matrix A of type T by vector B of type T columnwise.
+"""
 function dmv(A::GrB_Matrix{T}, B::GrB_Vector{T}) where T
     res = gbm_new(T, GrB_Matrix_nrows(A), GrB_Matrix_ncols(A))
     I, J, X = GrB_Matrix_extractTuples(A)
@@ -82,6 +124,11 @@ function dmv(A::GrB_Matrix{T}, B::GrB_Vector{T}) where T
     return res
 end
 
+"""
+    dmv_old(A, B)
+
+Same as `dmv` but much slower (Deprecated).
+"""
 function dmv_old(A::GrB_Matrix{T}, B::GrB_Vector{T}) where T
     @assert GrB_Matrix_ncols(A) == GrB_Vector_size(B)
     res = gbm_new(GrB_Matrix_nrows(A), GrB_Matrix_ncols(A))
@@ -104,12 +151,22 @@ function dmv_old(A::GrB_Matrix{T}, B::GrB_Vector{T}) where T
     return res2
 end
 
+"""
+    div_by_two(A)
+
+Element-wise division by two of matrix A, creating a new matrix as result.
+"""
 function div_by_two(A::GrB_Matrix{T}) where T
     res = gbm_new(T, GrB_Matrix_nrows(A), GrB_Matrix_ncols(A))
     GrB_apply(res, GrB_NULL, GrB_NULL, DIV_BY_TWO(T), A, GrB_NULL)
     return res
 end
 
+"""
+    div_by_two!(A)
+
+Element-wise division by two of matrix A.
+"""
 function div_by_two!(A::GrB_Matrix{T}) where T
     GrB_apply(A, GrB_NULL, GrB_NULL, DIV_BY_TWO(T), A, GrB_NULL)
 end
